330060264fd0bbc1763476d5de8ba92cc21dc88f
Michael Zikes <michael.zikes@intauria.com>
2020-10-09 14:43:55 +0200

    Playground

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent  {
   title = 'Hello Angular!';
+  myNgMsg = 'Yeah, we are using Bindings! :)';
+
+  handleTitleChange(): void {
+    this.title = 'Hello World!';
+  }
 }
