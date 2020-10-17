0e3f1ad098b53f871939dd36aa216095c4e90fe7
Michael Zikes <michael.zikes@intauria.com>
2020-10-17 11:59:23 +0200

    Pipes

--- src/app/shared/pipes/city.pipe.ts
+++ src/app/shared/pipes/city.pipe.ts
@@ -1,12 +1,36 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Pipe({
   name: 'city'
 })
 export class CityPipe implements PipeTransform {
 
-  transform(value: string): string {
-    return value + ' (transformed)';
-  }
+  transform(value: string, fmt: string): Observable<string> {
+    let long: string;
+    let short: string;
 
+    switch (value) {
+      case 'Graz':
+        long = 'Flughafen Graz Thalerhof';
+        short = 'GRZ';
+        break;
+      case 'Hamburg':
+        short = 'HAM';
+        long = 'Airport Hamburg Fulsbüttel Helmut Schmidt';
+        break;
+      case 'Wien':
+        short = 'VIE';
+        long = 'Flughafen Wien Schwechat';
+        break;
+      default:
+        short = long = value;
+    }
+
+    if (fmt === 'short') {
+      return of(short);
+    }
+
+    return of(long);
+  }
 }
